7eeb78d7a5e45a25ba33ed25c36e1a4ddaf01e4f
tiagoslucas <tiago.slucas@gmail.com>
2017-02-16 23:27:26 +0000

    Add files via upload

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    exec(open("initdb.py").read())
+    return render_template("home.html")
+
+@app.route('/enternew')
+def enternew():
+    return render_template("food.html")
+
+@app.route('/addfood', methods = ["POST"])
+def addfood():
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+    
+    try:
+        name = request.form['name']
+        calories = request.form['calories']
+        cuisine = request.form['cuisine']
+        vegetarian = request.form['is_vegetarian']
+        gluten = request.form['is_gluten_free']
+        cursor.execute('INSERT INTO foods (name,calories,cuisine,is_vegetarian,is_gluten_free) VALUES (?, ?, ?, ?, ?)', (name, calories, cuisine, vegetarian, gluten))
+        connection.commit()
+        message = "Record sucessfully added."
+    except:
+        connection.rollback()
+        message = "Error in database insertion."
+    finally:
+        connection.close()
+        return render_template("result.html", message = message)
